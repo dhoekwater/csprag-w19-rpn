8a110cc042fe6f19dbaa58cdb63b24cc6870bd67
Daniel Hoekwater <danieh65@gmail.com>
2019-03-13 22:05:53 -0400

    Add logic for RPN calculaor

--- rpn.py
+++ rpn.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python3
 
-def calculate(arg):
-    pass
+import math
+import operator
+operators = {'+'  : operator.add,
+             '-'  : operator.sub,
+             '*'  : operator.mul,
+             '/'  : operator.truediv,
+             '^'  : operator.pow,
+             '//' : operator.floordiv
+        }
 
+def is_numeric(text):
+    try:
+        float(text)
+        return True
+    except ValueError:
+        return False
+
+def calculate(arg):
+    calc_stack = []
+    for token in arg.split(' '):
+        if is_numeric(token):
+            calc_stack.append(float(token))
+        else:
+            a = calc_stack.pop()
+            b = calc_stack.pop()
+            return operators[token](a, b)
 
 def main():
     while True:
--- test_rpn.py
+++ test_rpn.py
@@ -6,5 +6,9 @@ class TestBasics(unittest.TestCase):
         result = rpn.calculate("1 1 +")
         self.assertEqual(2, result)
 
+    def test_exponent(self):
+        result = rpn.calculate("10 2 ^")
+        self.assertEqual(1024, result)
+
 #TestBasics tb;
 
